882d86d563de78435ef7dbb0c00321e565c6a370
zunfa.zhong <973373278@qq.com>
2018-04-03 13:09:49 +0800

    update securite config.

--- ch11-2-config-client-refresh/src/main/java/cn/springcloud/book/config/client/config/ConfigInfoProperties.java
+++ ch11-2-config-client-refresh/src/main/java/cn/springcloud/book/config/client/config/ConfigInfoProperties.java
@@ -1,6 +1,7 @@
 package cn.springcloud.book.config.client.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 /**
@@ -9,10 +10,11 @@ import org.springframework.stereotype.Component;
  * @author: zzf
  * @date: 2018/3/16
  * @time: 23:55
- * @description : 配置信息
+ * @description : 配置信息, 这里需要注意的是这个单独的配置类， 需要在上面添加refresh 注解
  */
 @Component
 @ConfigurationProperties(prefix = "cn.springcloud.book")
+@RefreshScope
 public class ConfigInfoProperties {
 
     private String config;
--- ch11-2-config-client-refresh/src/main/java/cn/springcloud/book/config/client/config/SecurityConfiguration.java
+++ ch11-2-config-client-refresh/src/main/java/cn/springcloud/book/config/client/config/SecurityConfiguration.java
@@ -12,7 +12,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.requestMatcher(EndpointRequest.toAnyEndpoint()).authorizeRequests()
-				.anyRequest().permitAll();
+		http.csrf().disable();
 	}
 }
